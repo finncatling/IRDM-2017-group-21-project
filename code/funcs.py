bca7f45650a9e391ac9e9cc37bd0a0671070cfcf
Finn Catling <finn.catling@gmail.com>
2017-04-14 12:25:47 +0100

    first draft (not working yet)

--- code/funcs.py
+++ code/funcs.py
@@ -1,75 +1,82 @@
-import re
-from nltk.stem.porter import *
-from sklearn.metrics import mean_squared_error, make_scorer
-stemmer = PorterStemmer()
+import string
 import nltk
-from spelling import *
-print(nltk.__version__,'<- needs to be less than 3.2.2')
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+from sklearn.metrics import mean_squared_error, make_scorer
+from code.spelling import *
 
-def word_stem(words): 
 
+stemmer = PorterStemmer()
+stops = set(stopwords.words("english"))
+punct = set(string.punctuation)
+print(nltk.__version__, '<- needs to be less than 3.2.2')
+
+
+def process_words(words, stem=True, remove_stopwords=True, remove_punctuation=True):
     if isinstance(words, str):
-
         words = words.lower()
         
-        # checks phrases
+        # spellcheck phrases
         if words in external_data_dict.keys():
-
             words = external_data_dict[words]
 
-        # checks words
-        words = (" ").join([external_data_dict[z] if z in external_data_dict.keys() else z for z in words.split(" ")])
+        # tokenize, so now words is a list, not a str
+        words = nltk.word_tokenize(words)
+
+        if remove_stopwords:
+            words = [z for z in words if z not in stops]
+
+        if remove_punctuation:
+            words = [z for z in words if z not in punct]
+
+        # spellcheck words
+        words = [external_data_dict[z] if z in external_data_dict.keys() else z for z in words]
+
+        if stem:
+            words = [stemmer.stem(z) for z in words]
         
-        words = (" ").join([stemmer.stem(z) for z in words.split(" ")])
-        
-        return words.lower()
+        return words
+
     else:
         return "null"
 
 
-
-def word_intersection(term1, term2):
-
-    words =  term1.split()
-    count = 0 
-
-    for word in words:
-
-        if term2.find(word)>=0:
-
-            count+=1
-
-    return count
-
-
-
-def term_intersection(term1, term2):
-
+def term_intersection(needle, haystack):
+    term1, term2 = " ".join(needle), " ".join(haystack)
     count = 0
     counter = 0
-    
-    while counter < len(term2):
 
+    while counter < len(term2):
         counter = term2.find(term1, counter)
 
         if counter == -1:
-
             return count
 
         else:
             count += 1
-
             counter += len(term1)
 
     return count
 
 
+def word_intersection(needles, haystack):
+    count = 0
+    for needle in needles:
+        if haystack.count(needle) > 0:
+            count += 1
+    return count
+
+
+def count_letters(words):
+    count = 0
+    for word in words:
+        count += len(word)
+    return count
+
 
 def ms_error(y, y_hat):
-
     ms_error_cal = mean_squared_error(y, y_hat)**0.5
-
     return ms_error_cal
 
-RMSE  = make_scorer(ms_error, greater_is_better=False)
 
+RMSE = make_scorer(ms_error, greater_is_better=False)
--- code/pre_processing.py
+++ code/pre_processing.py
@@ -1,10 +1,16 @@
 import numpy as np
 import pandas as pd
-import funcs as f
+import code.funcs as f
 import time
 import pickle
-from spelling import *
+from sklearn.utils import shuffle
 
+
+stem = True  # consider skipping in neural models as GloVe embeddings are mapped to words
+remove_stopwords = True  # consider skipping in neural models
+remove_punctuation = True  # consider skipping in neural models
+
+seed = 1
 start = time.time()
 
 # train and test sets
@@ -15,14 +21,15 @@ x_test = pd.read_csv('../../data/test.csv', encoding="ISO-8859-1")
 attributes = pd.read_csv('../../data/attributes.csv')
 
 # extract brand from attributes
-brand = attributes[attributes.name == "MFG Brand Name"][["product_uid", "value"]].rename(columns={"value": "brand"})
+brand = attributes[attributes.name == "MFG Brand Name"][["product_uid", "value"]].rename(
+    columns={"value": "brand"})
 
 # product descriptions
 description = pd.read_csv('../../data/product_descriptions.csv')
 
 # separate input and labels
 y_train = x_train['relevance']
-x_train =  x_train.drop('relevance',axis=1)
+x_train = x_train.drop('relevance', axis=1)
 
 # concatenate train and test
 x_all = pd.concat((x_train, x_test), axis=0, ignore_index=True)
@@ -32,37 +39,53 @@ x_all = pd.merge(x_all, description, how='left', on='product_uid')
 x_all = pd.merge(x_all, brand, how='left', on='product_uid')
 
 # define corpora
-x_all['search_term'] = x_all['search_term'].map(lambda x:f.word_stem(x))
-x_all['product_title'] = x_all['product_title'].map(lambda x:f.word_stem(x))
-x_all['product_description'] = x_all['product_description'].map(lambda x:f.word_stem(x))
-x_all['brand'] = x_all['brand'].map(lambda x:f.word_stem(x))
+x_all['search_term'] = x_all['search_term'].map(
+    lambda x: f.process_words(x, stem, remove_stopwords, remove_punctuation))
+x_all['product_title'] = x_all['product_title'].map(
+    lambda x: f.process_words(x, stem, remove_stopwords, remove_punctuation))
+x_all['product_description'] = x_all['product_description'].map(
+    lambda x: f.process_words(x, stem, remove_stopwords, remove_punctuation))
+x_all['brand'] = x_all['brand'].map(
+    lambda x: f.process_words(x, stem, remove_stopwords, remove_punctuation))
 
-### create length features
-x_all['len_of_query'] = x_all['search_term'].map(lambda x:len(x.split())).astype(np.int64)
-x_all['len_of_title'] = x_all['product_title'].map(lambda x:len(x.split())).astype(np.int64)
-x_all['len_of_description'] = x_all['product_description'].map(lambda x:len(x.split())).astype(np.int64)
-x_all['len_of_brand'] = x_all['brand'].map(lambda x:len(x.split())).astype(np.int64)
+# create length features
+x_all['len_of_query'] = x_all['search_term'].map(lambda x: len(x)).astype(np.int64)
+x_all['len_of_title'] = x_all['product_title'].map(lambda x: len(x)).astype(np.int64)
+x_all['len_of_description'] = x_all['product_description'].map(lambda x: len(x)).astype(np.int64)
+x_all['len_of_brand'] = x_all['brand'].map(lambda x: len(x)).astype(np.int64)
 
-# concatenate corpora serach term, product title and product description / brand is defined separately in attr
-x_all['product_info'] = x_all['search_term']+"\t"+x_all['product_title'] +"\t"+x_all['product_description']
-x_all['attr'] = x_all['search_term']+"\t"+x_all['brand']
+# does search term appear in: (brand added no information)
+x_all['query_in_title'] = x_all.apply(
+    lambda x: f.term_intersection(x['search_term'], x['product_title']))
+x_all['query_in_description'] = x_all.apply(
+    lambda x: f.term_intersection(x['search_term'], x['product_description']))
 
-### does search term appear in: (brand added no information)
-x_all['query_in_title'] = x_all['product_info'].map(lambda x:f.term_intersection(x.split('\t')[0],x.split('\t')[1]))
-x_all['query_in_description'] = x_all['product_info'].map(lambda x:f.term_intersection(x.split('\t')[0],x.split('\t')[2]))
+# how many common words
+x_all['word_in_title'] = x_all.apply(
+    lambda x: f.word_intersection(x['search_term'], x['product_title']))
+x_all['word_in_description'] = x_all.apply(
+    lambda x: f.word_intersection(x['search_term'], x['product_description']))
+x_all['word_in_brand'] = x_all.apply(
+    lambda x: f.word_intersection(x['search_term'], x['brand']))
 
-### how many common words
-x_all['word_in_title'] = x_all['product_info'].map(lambda x:f.word_intersection(x.split('\t')[0],x.split('\t')[1]))
-x_all['word_in_description'] = x_all['product_info'].map(lambda x:f.word_intersection(x.split('\t')[0],x.split('\t')[2]))
-x_all['word_in_brand'] = x_all['attr'].map(lambda x:f.word_intersection(x.split('\t')[0],x.split('\t')[1]))
+# concatenate corpora search term, product title and product description
+# brand is defined separately in attr
+# x_all['product_info'] = (
+#     x_all['search_term'] +
+#     ["\t"] +
+#     x_all['product_title'] +
+#     ["\t"] +
+#     x_all['product_description']
+# )
+# x_all['attr'] = x_all['search_term'] + ["\t"] + x_all['brand']
 
-### ratio of words in target to search term
-x_all['ratio_title'] = x_all['word_in_title']/x_all['len_of_query']
-x_all['ratio_description'] = x_all['word_in_description']/x_all['len_of_query']
-x_all['ratio_brand'] = x_all['word_in_brand']/x_all['len_of_brand']
+# ratio of words in target to search term
+x_all['ratio_title'] = x_all['word_in_title'] / x_all['len_of_query']
+x_all['ratio_description'] = x_all['word_in_description'] / x_all['len_of_query']
+x_all['ratio_brand'] = x_all['word_in_brand'] / x_all['len_of_brand']
 
-### letters in search term
-x_all['search_term_feature'] = x_all['search_term'].map(lambda x:len(x))
+# letters in search term
+x_all['search_term_feature'] = x_all['search_term'].map(lambda x: f.count_letters(x))
 
 print('x_all shape', x_all.shape)
 
@@ -76,6 +99,10 @@ x_train = x_all[:train_size]
 x_test = x_all[train_size:]
 print('x_train shape', x_train.shape)
 print('x_test shape', x_test.shape)
+
+# shuffle data
+x_train, y_train = shuffle(x_train, y_train, random_state=seed)
+x_test = shuffle(x_test, random_state=seed)
 
 # pickle the data
 pickle_file = '../../data/pre_processed_data.pickle'
@@ -91,6 +118,3 @@ f.close()
 
 duration = time.time() - start
 print(duration)
-
-
-
