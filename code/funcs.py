bb7b2dc6313ea573a7beb082f70b954ff9576de4
indra-n <indra.niedre@gmail.com>
2017-04-17 12:40:33 +0100

    k-folds cross validation
    Added a function to get data sets for k-fold cross validation and the
    file I used to find best column for PRank algorithm

--- code/funcs.py
+++ code/funcs.py
@@ -161,6 +161,35 @@ def split_val_set(x_, y_, val_ratio, col_name, seed=None):
     return x_train, y_train, x_val, y_val
 
 
+def get_k_folds(k, x_, y_, col_name):
+    """
+        Return the data set split into k folds
+        :param k: number of folds
+        :param x_: pandas data frame as loaded from pickle after pre-processing
+        :param y_: pandas series of true scores as loaded from pickle
+        :param col_name: name of column to split on e.g. 'product_uid'
+        :return: a list of folds. each fold contains 4 elements - x_train, y_train, x_val, y_val
+        """
+    val_sets = []
+    x_t = x_
+    y_t = y_
+    for i in range(k-1):
+        ratio_ = 1/(k-i)
+        x_train, y_train, x_val, y_val = split_val_set(x_t, y_t, ratio_, col_name)
+        val_sets.append([x_val, y_val])
+        x_t = x_train
+        y_t = y_train
+    val_sets.append([x_t, y_t]) # the last iteration split the set in half
+    folds = []
+    for i in range(k):
+        train_list_x = [val_sets[j][0] for j in range(len(val_sets)) if j != i] # all other folds will be used for training
+        train_list_y = [val_sets[j][1] for j in range(len(val_sets)) if j != i]
+        x_train = pd.concat(train_list_x, axis=0, ignore_index=True)
+        y_train = pd.concat(train_list_y, axis=0, ignore_index=True)
+        folds.append([x_train, y_train, val_sets[i][0], val_sets[i][1]])
+    return folds
+
+
 def word_intersection(term1, term2):
     words = term1.split()
     count = 0
